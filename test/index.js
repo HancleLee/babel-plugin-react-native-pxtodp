2a6793c08ef83f8b83f34c32edc2ccec1044514f
lihuanke <lihuanke@sensetime.com>
2023-11-23 16:24:59 +0800

    feat: 替换StyleSheet.create的样式

---
+++ test/index.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native'
+import { b } from 'ff'
+function test() {
+    console.log('test', b)
+}
+
+const s = StyleSheet.create({
+    main: {
+        height: 23,
+        width: 10,
+        a: 88,
+    },
+    width: {
+        width: 1,
+        y: 2
+    }
+})
+
+const tp = {
+    main: {
+        width: 1,
+        y: 2
+    }
+}
